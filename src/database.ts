432089119f1dac10cf02e4cd4824e764416297bc
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-10-29 19:51:36 -0300

    Create .env config and install packages zod and dotenv to validate env variables

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
-import { knex as setupKnex } from "knex";
+import { knex as setupKnex, Knex } from "knex";
+import { env } from './env';
 
-const config = {
-    client: 'sqlite',
-    connection: {
-        filename: './../database/database.sqlite'
-    },
+const config: Knex.Config = {
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_CLIENT === 'sqlite' ? {
+        filename: env.DATABASE_URL
+    } : env.DATABASE_CLIENT,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
